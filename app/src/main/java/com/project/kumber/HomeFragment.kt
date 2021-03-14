19e6f2e8b5c6fcd60ba946e685b36de81856cd8e
anggara catra <anggara663@gmail.com>
2021-03-15 01:15:29 +0700

    intent HomeFragment-NewsFragment

--- app/src/main/java/com/project/kumber/HomeFragment.kt
+++ app/src/main/java/com/project/kumber/HomeFragment.kt
@@ -1,12 +1,43 @@
 package com.project.kumber
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.cardview.widget.CardView
+import androidx.fragment.app.Fragment
 
 class HomeFragment : Fragment() {
 
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val v = inflater.inflate(R.layout.fragment_home, container, false)
+        val clickBtn1 = v.findViewById<View>(R.id.button1) as CardView
+        val clickBtn2 = v.findViewById<View>(R.id.button2) as CardView
+        val clickBtn3 = v.findViewById<View>(R.id.button3) as CardView
+        val clickBtn4 = v.findViewById<View>(R.id.button4) as CardView
 
+        clickBtn1.setOnClickListener{
+            val frNews = fragmentManager!!.beginTransaction()
+            frNews.replace(R.id.fragment_container, NewsFragment())
+            frNews.addToBackStack(null).commit()
+        }
+        clickBtn2.setOnClickListener{
+            val frNews = fragmentManager!!.beginTransaction()
+            frNews.replace(R.id.fragment_container, NewsFragment())
+            frNews.addToBackStack(null).commit()
+        }
+        clickBtn3.setOnClickListener{
+            val frNews = fragmentManager!!.beginTransaction()
+            frNews.replace(R.id.fragment_container, NewsFragment())
+            frNews.addToBackStack(null).commit()
+        }
+        clickBtn4.setOnClickListener{
+            val frNews = fragmentManager!!.beginTransaction()
+            frNews.replace(R.id.fragment_container, NewsFragment())
+            frNews.addToBackStack(null).commit()
+        }
+
+        return v
+    }
 }
--- app/src/main/java/com/project/kumber/MainActivity.kt
+++ app/src/main/java/com/project/kumber/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.project.kumber
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, HomeFragment()).commit()
 
         val homeFragment = HomeFragment();
         //initialize fragmentmu
