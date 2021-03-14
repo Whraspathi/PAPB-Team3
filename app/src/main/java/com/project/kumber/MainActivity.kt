a1611ff0f89d473f45ca0e7a3bba324a9393325a
Whraspathi <71971758+Whraspathi@users.noreply.github.com>
2021-03-14 23:37:02 +0700

    Layout Home

--- app/src/main/java/com/project/kumber/MainActivity.kt
+++ app/src/main/java/com/project/kumber/MainActivity.kt
@@ -7,5 +7,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val homeFragment = HomeFragment();
+        //initialize fragmentmu
+
+        //
+        supportFragmentManager.beginTransaction().apply {
+
+        }
     }
 }
