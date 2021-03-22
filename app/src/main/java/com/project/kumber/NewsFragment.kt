2863984c8e5764916e9e42c22fffe282e2e53696
anggara catra <anggara663@gmail.com>
2021-03-22 11:36:42 +0700

    onResume function

--- app/src/main/java/com/project/kumber/NewsFragment.kt
+++ app/src/main/java/com/project/kumber/NewsFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 
@@ -25,5 +26,9 @@ class NewsFragment : Fragment() {
         }
 
         return v
+    }
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(activity, "Berita Diperbaharui", Toast.LENGTH_LONG).show()
     }
 }
