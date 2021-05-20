d6beef24e69d8f576756c351011614b7622caec7
adravel <arrafly.ad@gmail.com>
2021-05-20 22:07:46 +0700

    Add ProviderFactory and update MainActivity.kt

--- app/src/main/java/com/project/kumber/ui/MainActivity.kt
+++ app/src/main/java/com/project/kumber/ui/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.project.kumber.repository.NewsRepository
-import com.project.kumber.ui.NewsViewModel
 import com.project.kumber.ui.fragment.HomeFragment
 import com.project.kumber.R
 import com.project.kumber.db.ArticleDatabase
@@ -16,6 +15,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val newsRepository = NewsRepository(ArticleDatabase(this))
+        val viewModelProviderFactory = NewsViewModelProviderFactory(newsRepository)
+        viewModel  = ViewModelProvider(this, viewModelProviderFactory).get(NewsViewModel::class.java)
 
         supportFragmentManager.beginTransaction().replace(R.id.fragment_container, HomeFragment()).commit()
 
---
+++ app/src/main/java/com/project/kumber/ui/NewsViewModelProviderFactory.kt
@@ -0,0 +1,14 @@
+package com.project.kumber.ui
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.project.kumber.repository.NewsRepository
+
+class NewsViewModelProviderFactory(
+    val newsRepository: NewsRepository
+) : ViewModelProvider.Factory {
+
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return NewsViewModel(newsRepository) as T
+    }
+}
